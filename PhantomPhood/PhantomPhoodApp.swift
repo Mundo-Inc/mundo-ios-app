8aac0bab9a3e47fdef2e68f8832da9f84a43bc36
Kia Abdi <dr.kia99@gmail.com>
2023-09-11 20:26:47 +0300

    Initial Commit

---
+++ PhantomPhood/PhantomPhoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhantomPhoodApp.swift
+//  PhantomPhood
+//
+//  Created by Kia Abdi on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhantomPhoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
